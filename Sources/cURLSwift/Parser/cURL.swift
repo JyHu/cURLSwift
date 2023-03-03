c921d75845ce98c496f3a84e9e1549a0f94034ab
Jinyou Hu <hujinyou@itiger.com>
2023-03-03 23:28:34 +0800

    增加多curl的解析

--- Sources/cURLSwift/Parser/cURL.swift
+++ Sources/cURLSwift/Parser/cURL.swift
@@ -10,11 +10,11 @@ import Foundation
 public struct cURL {
     
     /// 所有的请求参数
-    public private(set) var options: [Option] = []
+    public fileprivate(set) var options: [Option] = []
     /// 参数字典，用于合并相同类型的参数，key为curl参数对应的uuid
-    public private(set) var optionMap: [OptionUUID: [Option]] = [:]
+    public fileprivate(set) var optionMap: [OptionUUID: [Option]] = [:]
     /// 请求的url
-    public private(set) var url: String?
+    public fileprivate(set) var url: String?
     
     /// 初始化方法
     /// - Parameter curl: curl
@@ -51,12 +51,15 @@ public struct cURL {
                 alikeOptions.append(option)
                 self.optionMap[identifier] = alikeOptions
             } else {
+                if token.uppercased() == "CURL" { return }
                 self.url = token
             }
             
             index += 1
         }
     }
+    
+    fileprivate init() { }
     
     public var description: String {
         var result = "curl "
@@ -86,5 +89,60 @@ public struct cURL {
         }
         
         return optionList
+    }
+}
+
+public struct cURLs {
+    public private(set) var curlObjs: [cURL] = []
+    
+    public init(curlString: String) throws {
+        /// 获取拆分curl中的所有的token
+        let tokens = try cURLParser.tokens(of: curlString)
+        
+        var curlObj = cURL()
+        
+        /// 遍历所有的token，并解析成curl option
+        var index: Int = 1
+        
+        while index < tokens.count {
+            let token = tokens[index]
+            
+            if token.uppercased() == "CURL" {
+                curlObjs.append(curlObj)
+                curlObj = cURL()
+                continue
+            }
+            
+            if token.hasPrefix("-") {
+                var (command, body) = token.divide()
+                
+                let optionKey = try OptionKey.from(command)
+                let unit = try Options.shared.unit(of: optionKey)
+                
+                if unit.hasArg && body == nil {
+                    index += 1
+                    
+                    guard index < tokens.count else {
+                        throw cURLError.indexOverFlow(index: index)
+                    }
+                    
+                    body = tokens[index]
+                }
+                
+                let option = try OptionFactory.shared.makeOption(with: optionKey, unit: unit, arg: body)
+                curlObj.options.append(option)
+                
+                let identifier = option.unit.identifier
+                var alikeOptions = curlObj.optionMap[identifier] ?? []
+                alikeOptions.append(option)
+                curlObj.optionMap[identifier] = alikeOptions
+            } else {
+                curlObj.url = token
+            }
+            
+            index += 1
+        }
+        
+        curlObjs.append(curlObj)
     }
 }
